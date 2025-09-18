d87c6b90a514ba0478021cd91a67d39e590045aa
AndresVillacobR <andresvillacobr@gmail.com>
2025-09-17 21:13:25 -0500

    hago cambio y implementacion de codigo

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, ANDRES VILLACOB!"
-
+# Ruta principal -> index.html
 @app.route('/index')
 def index():
-      return render_template ('Index.html')
+    return render_template('index.html')
 
-app.run(host='0.0.0.0' , port=5000, debug=True)
+# Ruta -> base.html
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+# Ruta -> mision.html
+@app.route('/mision')
+def mision():
+    return render_template('mision.html')
+
+# Ruta -> vision.html
+@app.route('/vision')
+def vision():
+    return render_template('vision.html')
+
+# Ruta -> sedes.html
+@app.route('/sedes')
+def sedes():
+    return render_template('sedes.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
